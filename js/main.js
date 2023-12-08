1dd00ce1c6df90dc22f2e0f5790c03fbb8a0446e
aleky63 <ab63@list.ru>
2023-12-08 12:18:51 +0300

    second08_12

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("burger").addEventListener("click", function () {
-    document.querySelector("header").classList.toggle("open");
+    document.querySelector(".header").classList.toggle("open");
   });
 });
 
